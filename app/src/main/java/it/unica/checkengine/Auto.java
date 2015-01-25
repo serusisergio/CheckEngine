48858b4d2a2209aed7223f15c50b38e93d7b8772
Stefano Lande <landeste@gmail.com>
2015-01-25 16:43:36 +0100

    modifica classe Auto

--- app/src/main/java/it/unica/checkengine/Auto.java
+++ app/src/main/java/it/unica/checkengine/Auto.java
@@ -1,5 +1,8 @@
 package it.unica.checkengine;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Stefano on 23/01/2015.
  */
@@ -9,11 +12,31 @@ public class Auto {
     private int livelloOlio ; /*la macchina ha sui 4 litri di olio */
     private int km ;
     private String nome;
+    private List<Manutenzione> manutenzioni;
+    private List<Tributo> tributi;
+    private List<Avaria> avarie;
+    private int kmGiornalieri;
 
     public Auto(){
         this.setCarburante(25);
         this.setLivelloOlio(4);
         this.setKm(0); /*supponiamo che l'abbia ppena comprata? */
+
+        this.manutenzioni = new LinkedList<>();
+        this .tributi = new LinkedList<>();
+        this.avarie = new LinkedList<>();
+    }
+
+    public void addManutenzione(Manutenzione m){
+        this.getManutenzioni().add(m);
+    }
+
+    public void addTributo(Tributo t){
+        this.getTributi().add(t);
+    }
+
+    public void addAvaria(Avaria a){
+        this.getAvarie().add(a);
     }
 
     public int getCarburante() {
@@ -42,5 +65,25 @@ public class Auto {
 
     public void setKm(int km) {
         this.km = km;
+    }
+
+    public int getKmGiornalieri() {
+        return kmGiornalieri;
+    }
+
+    public void setKmGiornalieri(int kmGiornalieri) {
+        this.kmGiornalieri = kmGiornalieri;
+    }
+
+    public List<Manutenzione> getManutenzioni() {
+        return manutenzioni;
+    }
+
+    public List<Tributo> getTributi() {
+        return tributi;
+    }
+
+    public List<Avaria> getAvarie() {
+        return avarie;
     }
 }
