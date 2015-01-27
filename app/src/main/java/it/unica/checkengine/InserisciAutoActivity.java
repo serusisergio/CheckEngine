acf723718fcfa301df493d0b2edbebb6bc607339
Riccardo S <askriccardo@outlook.com>
2015-01-27 23:29:15 +0100

    removed menu icon on CreateAuto

--- app/src/main/java/it/unica/checkengine/InserisciAutoActivity.java
+++ app/src/main/java/it/unica/checkengine/InserisciAutoActivity.java
@@ -34,7 +34,7 @@ public class InserisciAutoActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_inserisci_auto, menu);
+        //getMenuInflater().inflate(R.menu.menu_inserisci_auto, menu);
         return true;
     }
 
