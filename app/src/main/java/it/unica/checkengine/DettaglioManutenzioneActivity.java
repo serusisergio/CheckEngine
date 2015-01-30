fc7057cb700e48c33d4251fd7bc7e71a4fe6bfe4
Riccardo S <askriccardo@outlook.com>
2015-01-30 19:43:59 +0100

    Tasto indietro nella visione della manutenzione, chiamata al meccanico abilitata

--- app/src/main/java/it/unica/checkengine/DettaglioManutenzioneActivity.java
+++ app/src/main/java/it/unica/checkengine/DettaglioManutenzioneActivity.java
@@ -1,8 +1,11 @@
 package it.unica.checkengine;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,12 +23,11 @@ public class DettaglioManutenzioneActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dettaglio_manutenzione);
 
-
         garage = (Garage) getIntent().getSerializableExtra(ARG_GARAGE);
         Auto auto = garage.getAuto();
         Manutenzione manutenzione = new Manutenzione();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //imposto il titolo della actionbar come da prototipo
         setTitle(auto.getModello() + " - " + auto.getNome());
@@ -51,25 +53,32 @@ public class DettaglioManutenzioneActivity extends ActionBarActivity {
         TextView messaggio = (TextView) findViewById(R.id.text_messaggio);
         messaggio.setText(manutenzione.getMessaggio());
 
-
-        Button indietro = (Button) findViewById(R.id.button_indietro);
-        indietro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyCarActivity.class);
-                intent.putExtra(GarageActivity.EXTRA_MESSAGE, garage.getAuto().getTarga());
-                startActivity(intent);
-            }
-        });
-
         Button meccanico = (Button) findViewById(R.id.button_meccanico);
         meccanico.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Sto chiamando il meccanico", Toast.LENGTH_SHORT).show();
+                String numM = "tel:"+garage.getNumMeccanico();
+                Log.d("eh",numM);
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(numM));
+                startActivity(intent);
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(getApplicationContext(), MyCarActivity.class);
+                intent.putExtra(GarageActivity.EXTRA_MESSAGE, garage.getAuto().getTarga());
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
